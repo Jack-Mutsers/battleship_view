e0c217d31773e5b8df31ea8821cdd349108d49d5
Jack <jmutsers95@hotmail.com>
2020-04-01 14:12:00 +0200

    first commit

---
+++ battleship_view/Pages/JoinSession.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace battleship_view
+{
+    public class JoinSessionModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
