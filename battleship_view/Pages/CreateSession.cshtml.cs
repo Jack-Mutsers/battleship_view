a3844492434b7a19c95dd01ec7623de44b424df2
Zoë Snijders <zoesni@live.nl>
2020-04-08 15:08:41 +0200

    Layout aangepast en backbutton geïmplementeerd

--- battleship_view/Pages/CreateSession.cshtml.cs
+++ battleship_view/Pages/CreateSession.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text;
 
 namespace battleship_view
 {
@@ -13,5 +14,25 @@ namespace battleship_view
         {
 
         }
+
+        public static void SessionCode()
+        {
+            int length = 5;
+
+            // creating a StringBuilder object()
+            StringBuilder str_build = new StringBuilder();
+            Random random = new Random();
+
+            char letter;
+
+            for (int i = 0; i < length; i++)
+            {
+                double flt = random.NextDouble();
+                int shift = Convert.ToInt32(Math.Floor(25 * flt));
+                letter = Convert.ToChar(shift + 65);
+                str_build.Append(letter);
+            }
+        }
+
     }
 }
