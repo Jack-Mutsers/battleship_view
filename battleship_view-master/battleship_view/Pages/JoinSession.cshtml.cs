182a23cc96856339de8df082a8db47b5c8b68518
Youri <youri.yvdl@gmail.com>
2020-04-09 10:55:08 +0200

    Worked on storing user input, Onpost() reverses the javascript which needs to be fixed

---
+++ battleship_view-master/battleship_view/Pages/JoinSession.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace battleship_view
+{
+    public class JoinSessionModel : PageModel
+    {
+        [BindProperty]
+        public string Username { get; set; }
+        public string Lobbycode { get; set; }
+
+        public List<string> Players = new List<string>();
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            Username = Request.Form["Username"];
+            Lobbycode = Request.Form["Lobbycode"];
+            //send lobbycode and username to the connection class
+            //receive the list of players
+            //Players = players;
+            Players.Add("youri");
+            Players.Add("lean");
+            Players.Add("martin");
+            Players.Add("jack");
+            Players.Add("maikel");
+            Players.Add("zoe");
+        }
+    }
+}
