84d0cdc150ed59f9a152d914a28c7f6fd4b12e50
jack mutsers <jmutsers95@hotmail.com>
2020-04-21 11:47:12 +0200

    updated Coördinates selection + moved javascript

---
+++ battleship_view/wwwroot/js/PlayingField.js
@@ -0,0 +1,20 @@
+﻿var Id;
+
+function SelectId(_Id) {
+
+    Id = _Id;
+    document.getElementById(_Id).style.fontSize = '24px';
+
+
+}
+
+function myFunction() {
+    document.getElementById("testtest").style.color = "red";
+}
+
+function GetCoordinates(element) {
+    var fieldnr = $(element).data("field");
+    var col = $(element).data("col");
+    var row = $(element).data("row");
+    alert("field: " + fieldnr + " -- col: " + col + " -- row: " + row);
+}
